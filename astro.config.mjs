8c574546c446615f36db73413bade5b075416b39
Edu Armas <edu_armas11@hotmail.com>
2025-10-13 17:03:48 -0500

    cambios sitemaps - plugin astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://formatosintergraficos.pe', // Cambia esto por tu dominio real
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
